621650a6ea206929b33cfacec1e24d111b741926
AndruPol <a_pol@hotbox.ru>
2014-05-26 13:58:42 +0400

    change files encoding to utf8

--- UET_STM32_F103/board.mk
+++ UET_STM32_F103/board.mk
@@ -1,5 +1,5 @@
 # List of all the board related files.
-BOARDSRC = ${CHIBIOS}/boards/UET_STM32_F103/board.c
+BOARDSRC = UET_STM32_F103/board.c
 
 # Required include directories
-BOARDINC = ${CHIBIOS}/boards/UET_STM32_F103
+BOARDINC = UET_STM32_F103
--- main.h
+++ main.h
@@ -11,15 +11,15 @@
 extern "C" {
 #endif
 
-#define FIRMWARE			"0.0.2"	// версия прошивки
-#define BMP085_DELAY_MS		1500		// интервал таймера задержки перед опросом датчика, мсек
+#define FIRMWARE			"0.0.2"	// РІРµСЂСЃРёСЏ РїСЂРѕС€РёРІРєРё
+#define BMP085_DELAY_MS		1500		// РёРЅС‚РµСЂРІР°Р» С‚Р°Р№РјРµСЂР° Р·Р°РґРµСЂР¶РєРё РїРµСЂРµРґ РѕРїСЂРѕСЃРѕРј РґР°С‚С‡РёРєР°, РјСЃРµРє
 
 // message format
 typedef struct MESSAGE MESSAGE_T;
 struct MESSAGE{
 	uint8_t msgType;	// message type: 0 - info, 1 - sensor value, 2 - sensor error
 	uint8_t errorCode;	// error code for msgType == 0: 0 - normal level, 1 - critical battery level
-	uint8_t sensorType;	// sensor type: 0 - 1-wire, 1 - BH1750, 2 - DHT сенсор, 3 - BMP085
+	uint8_t sensorType;	// sensor type: 0 - 1-wire, 1 - BH1750, 2 - DHT СЃРµРЅСЃРѕСЂ, 3 - BMP085
 	uint8_t valueType;	// value type: 0 - temperature, 1 - humidity, 2 - pressure
 	uint8_t owkey[8];	// sensor id for 1-wire, sensor number for DHT in owkey[0]
 	union	{			// sensor value depend of sensor type
